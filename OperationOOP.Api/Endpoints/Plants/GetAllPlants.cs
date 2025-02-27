639c99c614c10128ab414ecde9c8aef465c3367a
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-02-27 10:06:56 +0100

    Created GetAllPlants endpoint, which retrieves all plants and maps them to DTO

---
+++ OperationOOP.Api/Endpoints/Plants/GetAllPlants.cs
@@ -0,0 +1,44 @@
+﻿using OperationOOP.Core.Interfaces;
+using static OperationOOP.Core.Models.Plant;
+
+namespace OperationOOP.Api.Endpoints.Plants
+{
+    public class GetAllPlants : IEndpoint
+    {
+        // Mapping
+        public static void MapEndpoint(IEndpointRouteBuilder app) => app
+            .MapGet("/plants", Handle);
+
+
+
+        // Response
+        public record Response(
+            int Id,
+            string Species,
+            DateTime LastWatered,
+            DateTime LastPruned,
+            PlantCareLevel CareLevel
+            );
+
+
+
+        // Logic
+
+        private static List<Response> Handle(IPlantService plantService)
+        {
+            var plants = plantService.GetAll();
+            
+            plants.Select ( item  => new Response(
+                Id : item.Id,
+                Species: item.Species,
+                LastWatered: item.LastWatered,
+                LastPruned: item.LastPruned,
+                CareLevel: item.CareLevel                
+                )).ToList();
+        
+
+
+
+        }
+    }
+}
